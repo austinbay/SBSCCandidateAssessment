8a8820f0817fd17e61d51ed600af14fe71f2caa5
Austin <austinokpakokomo@outlook.com>
2020-07-17 04:18:02 +0100

    new

---
+++ ConsecutiveElementProgram/ConsecutiveElement.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsecutiveElementProgram
+{
+    //Problem
+    // Given a string of hyphen separated numbers return true where k is the same
+    // for all numbers otherwise return false. 
+
+    // k is the interval between a number and the next.
+
+    //Solution
+    //Simple solution will be to sort the array and check if elements are consecutive just by 
+    //iterative over array.
+
+    //Steps
+    //Find minimum and maximum element in the array.
+    //Check if max-min+1==n, if elements are consecutive then this condition should meet.
+    //Create a visited boolean array.
+    //Iterate over the array and check
+    //visited [arr[i]-min] is true, then return false as elements are repeated.
+    //mark the element visited
+
+    public class ConsecutiveElement
+    {
+
+        public static void Main(string[] args)
+        {
+            var cep = new ConsecutiveElement();
+            var inputString = "47-43-45-44-46";
+            int[] inputValues = inputString.Split('-').Select(int.Parse).ToArray();
+
+            if (cep.ContainsConsecutiveElements(inputValues, inputValues.Length))
+                Console.WriteLine("True");
+
+            else
+                Console.WriteLine("False");
+            return;
+        }
+        // This method checks if array elements are consecutive
+        public bool ContainsConsecutiveElements(int[] inputValues, int n)
+        {
+            if (n < 1)
+                return false;
+
+            int min = GetMinimum(inputValues, n);
+            int max = GetMaximum(inputValues, n);
+
+            if (max - min + 1 == n)
+            {
+                bool[] visited = new bool[inputValues.Length];
+                for (int i = 0; i < n; i++)
+                {
+                    if (visited[inputValues[i] - min] != false)
+                        return false;
+
+                    visited[inputValues[i] - min] = true;
+                }
+
+                return true;
+            }
+            return false;
+        }
+        // Method return minimum value
+        private int GetMinimum(int[] inputValues, int n)
+        {
+            int min = inputValues[0];
+            for (int i = 1; i < n; i++)
+                if (inputValues[i] < min)
+                    min = inputValues[i];
+            return min;
+        }
+
+        // Method return maximum value
+        private int GetMaximum(int[] inputValues, int n)
+        {
+            int max = inputValues[0];
+            for (int i = 1; i < n; i++)
+                if (inputValues[i] > max)
+                    max = inputValues[i];
+            return max;
+        }
+
+
+    }
+
+}
---
+++ FrequencyOfEachElement/FrequencyOfEachElement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrequencyOfEachElementProgram
+{
+	//Problem
+	//Given a string compute how many times a letter occurs in the string and
+	//return it formatted such that every non-repeated letter in the string is
+	//followed by its number of occurrences.
+
+	//Solution
+	//What we can do is, we keep a frequency array and loop through the array, and every time we find 
+	//any element we go to the frequency array and add 1 to the previous frequency of that element in the frequency array.
+    //After the loop ends, we are left with an array where at every index their frequency in the original array is present.
+    //And also the biggest plus point along with its efficiency is, We don’t necessarily need the array to be sorted.
+
+	public class FrequencyOfEachElement
+    {
+        public static void Main(string[] args)
+        {
+
+			var inputString = "occurrence";
+			
+			Dictionary<char, int> freqDic = SolveIterative(inputString);
+			var result = string.Empty;
+			foreach (char val in freqDic.Keys)
+			{
+				result += string.Format("{0}{1}", val, freqDic[val]);						
+			}
+
+			Console.WriteLine(result);
+
+		}
+
+		public static Dictionary<char, int> SolveIterative(string inputString)
+		{
+			Dictionary<char, int> freqDic = new Dictionary<char, int>();
+			var arr = inputString.Trim().ToLower().ToArray();
+			/* iterate through the array for contributing +1 
+			 * as a frequency of that element, every time it is encountered.*/
+			foreach (char val in arr)
+			{
+				if (!freqDic.ContainsKey(val))
+				{
+					/* if hashmap doesnt contains that element, 
+					 * this means this is the first time the element is encountered,
+					 * therefor freq of this element will be one for now.*/
+					freqDic[val] = 1;
+				}
+				else
+				{
+					/* if hashmap contains this element, 
+					 * so now its updated frequency will be its past frequency + 1.
+					 */
+					freqDic[val] = freqDic[val] + 1;
+				}
+			}
+			return freqDic;
+		}
+	}
+
+}
